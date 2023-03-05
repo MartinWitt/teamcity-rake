3c005f349a81a3b2c09da36a7e819cbada69e046
MartinWitt <wittlinger.martin@gmail.com>
2023-03-05 16:25:59 +0100

    Update RubySdkImpl.java

--- rake-runner-agent/src/jetbrains/buildServer/agent/ruby/impl/RubySdkImpl.java
+++ rake-runner-agent/src/jetbrains/buildServer/agent/ruby/impl/RubySdkImpl.java
@@ -64,7 +64,7 @@ public class RubySdkImpl implements RubySdk {
       if (SystemInfo.isWindows) {
         executableName.append(".exe");
       }
-      myExecutablePath = new File(myHome, "bin" + File.separator + executableName );
+      myExecutablePath = new File(myHome, "bin" + File.separator + executableName);
     }
   }
 
